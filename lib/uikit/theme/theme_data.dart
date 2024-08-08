00acd72e95d869331f4b5d9f887302cda2a55b02
sahland <sahland@mail.ru>
2024-08-09 00:46:55 +0300

    carkey: setting up app theme (dark\light)

---
+++ lib/uikit/theme/theme_data.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../uikit.dart';
+
+abstract class AppThemeData {
+  static const _lightColorScheme = AppColorScheme.light();
+  static const _darkColorScheme = AppColorScheme.dark();
+
+  static final lightTheme = ThemeData(
+    extensions: const [_lightColorScheme],
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+      brightness: Brightness.light,
+      primary: _lightColorScheme.primary
+    ),
+    scaffoldBackgroundColor: _lightColorScheme.background,
+    cardColor: _lightColorScheme.surface,
+    primaryColor: _lightColorScheme.primary,
+    textTheme: TextTheme(
+      bodyMedium: TextStyle(
+        color: _lightColorScheme.textField
+      )
+    ),
+    dialogBackgroundColor: _lightColorScheme.dialogBackgroundColor
+  );
+
+  static final darkTheme = ThemeData(
+    extensions: const [_darkColorScheme],
+    brightness: Brightness.dark,
+    colorScheme: ColorScheme.dark(
+      brightness: Brightness.dark,
+      primary: _darkColorScheme.primary,
+      surface: _darkColorScheme.surface
+    ),
+    scaffoldBackgroundColor: _darkColorScheme.background,
+    cardColor: _darkColorScheme.surface,
+    primaryColor: _darkColorScheme.primary,
+    textTheme: TextTheme(
+      bodyMedium: TextStyle(
+        color: _darkColorScheme.textField
+      )
+    ),
+    dialogBackgroundColor: _darkColorScheme.dialogBackgroundColor
+  );
+}
