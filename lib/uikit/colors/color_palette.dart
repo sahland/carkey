362e6040194823a005d26c75edd5ed0cc9574219
sahland <sahland@mail.ru>
2024-08-09 00:44:19 +0300

    carkey: add color palette for a dark and light theme

---
+++ lib/uikit/colors/color_palette.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+abstract class LightColorPalette {
+  static const white = Colors.white;
+
+  static const black = Colors.black;
+
+  static const lightScaffold = Color(0xFFF9FAFB);
+
+  static const treeGreen = Color(0xFF14B95E);
+
+  static const darkGreen = Color(0xFF16683B);
+
+  static const lightGrey = Color(0xFFEDEEEF);
+
+  static const textGrey = Color(0xFF8C9199);
+}
+
+abstract class DarkColorPalette {
+  static const white = Colors.white;
+
+  static const black = Colors.black;
+
+  static const treeGreen = Color(0xFF14B95E);
+
+  static const darkGreen = Color(0xFF16683B);
+
+  static const darkScaffold = Color(0xFF1C202B);
+
+  static const textGrey = Color(0xFF8C9199);
+
+  static const darkGrey = Color(0xFF343D53);
+
+  static const blackBanner = Color(0xFF343D53);
+}
